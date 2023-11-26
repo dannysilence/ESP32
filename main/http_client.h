515b053a9739e9a3b76ed304985c63f1b9d4ab32
dannysilence <danny.bondarenko@outlook.com>
2023-11-26 03:44:17 +0200

    Update http_client.h

--- main/http_client.h
+++ main/http_client.h
@@ -3,6 +3,6 @@ include <esp_err.h>
 #ifndef DB_ESP32_HTTP_CLIENT_NEW_H
 #define DB_ESP32_HTTP_CLIENT_NEW_H
 
-esp_err_t http_get(const char *url);
+int http_get(const char *url);
 
 #endif //DB_ESP32_HTTP_CLIENT_NEW_H
